e135c7f169c771e458a7a635d96553188c9709be
Nivetha-deve <nivethashree028@gmail.com>
2024-07-09 12:29:31 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <div className="Home">
+        <h1 style={{textAlign: "center"}}>Nivetha Shree</h1>
+        <h3 style={{textAlign: "center"}}>Full Stack Developer | MERN Stack Enthusiast</h3>
+        <p style={{textAlign: "center"}}>Welcome to my portfolio! I am Nivetha Shree, a skilled full stack developer specializing in the MERN stack.
+           With a passion for creating responsive web applications, I bring expertise in frontend development with React.js and Redux, backend development using Node.js and Express.js, and database management with MongoDB. 
+           Explore my projects and skills to see how I can contribute to your next development initiative.</p>
+           <a style={{marginLeft: 520}} href="file:///C:/Users/nivet/Downloads/Nivetha_Resume.pdf" target="_blank" rel="noopener noreferrer">View my Resume</a>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Pages/Project.jsx
@@ -0,0 +1,45 @@
+
+const Project = () => {
+    return (
+        <div className="project">
+        <h1>Project</h1>
+        <div className="project-main">
+        <section className="pro-1">
+               <h2>Music Streaming App</h2>
+               <h3>Full Project</h3>
+               <p>This music streaming application allows users to play and search for music based on various criteria such as artist, album, and song title. It features personalized playlists, high-quality streaming, and an intuitive user interface.</p>
+               <h4>Full Project:React.js, Bootstrap, MoongoDb,Node.js, Express</h4>
+               <a href="https://ornate-cactus-c8215b.netlify.app/login" target="_blank" rel="noopener noreferrer" className="button" >Live Demo</a>
+               <a href="https://github.com/Nivetha-deve/musicapp-frontend-capstone.git" target="_blank" rel="noopener noreferrer"  className="button">GitHub Repository</a>
+    </section>
+    <section className="pro-2">
+                <h2>Social Media Application</h2>
+                <h3>Full Project</h3>
+                <p>This social media application allows users to connect with friends, share updates, and engage with a community through posts, comments, and likes. It features user authentication, profile management, and real-time updates.</p>
+                <h4>Full Project:React.js, Bootstrap, MoongoDb,Node.js, Express</h4>
+                <a href="https://beamish-kangaroo-824e38.netlify.app/login" target="_blank" rel="noopener noreferrer"  className="button">Live Demo</a>
+                <a href="https://github.com/Nivetha-deve/frontend-social-media" target="_blank" rel="noopener noreferrer"  className="button">GitHub Repository</a>
+    </section>
+    <section className="pro-3">
+                <h2>ToDo Application</h2>
+                <h3>Front-end Project</h3>
+                <p>The Todo List application is a showcase of my skills in full-stack development, employing the CRUD (Create, Read, Update, Delete) methodology to enable users to manage their tasks effectively. Developed with a responsive design, the application ensures seamless usability across devices, prioritizing user experience.</p>
+                <h4>Full Project:React.js, Bootstrap</h4>
+                <a href="https://singular-daifuku-c6a964.netlify.app/" target="_blank" rel="noopener noreferrer"  className="button">Live Demo</a>
+                <a href="https://github.com/Nivetha-deve/B54WE-react-task-4.git" target="_blank" rel="noopener noreferrer"  className="button">GitHub Repository</a>
+    </section>
+    <section className="pro-4">
+                <h2>Shooping Cart Application</h2>
+                <h3>Front-end Project</h3>
+                <p>The Shopping Cart application showcases my proficiency in full-stack development, featuring Redux for efficient state management. Built with a focus on user experience, the application allows seamless browsing and purchasing of products. Utilizing Redux ensures streamlined data flow and centralized state management, enhancing scalability and performance. 
+                    The application responsive design ensures accessibility across various devices, providing a smooth shopping experience.</p>
+                <a href="https://vocal-faun-e010a5.netlify.app/" target="_blank" rel="noopener noreferrer"  className="button">Live Demo</a>
+                <a href="https://github.com/Nivetha-deve/B54WE_react_task_8.git" target="_blank" rel="noopener noreferrer"  className="button">GitHub Repository</a>
+    </section>
+
+    </div>
+    </div>
+    )
+}
+
+export default Project;
